5a1b53e209908daef0b9d6b2e1f83e61e58b7708
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2017-09-26 03:37:28 +0300

    -#1: Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -56,10 +56,10 @@ module.exports = {
         ]
     },
 
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM'
-    },
+    // externals: {
+    //     'react': 'React',
+    //     'react-dom': 'ReactDOM'
+    // },
 
     plugins: [
         new ExtractTextPlugin("style.css"),
